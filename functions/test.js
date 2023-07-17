7ebf425723df3d1b08458116a18b3c3b8b1699c8
xyinghu <xyinghu@tencent.com>
2023-07-17 14:23:17 +0800

    测试应用

---
+++ functions/test.js
@@ -0,0 +1,27 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(req) {
+    const method = req.method;
+    log("req.method:", method, req);
+    try {
+        const rspData = {
+            time: new Date().getTime(),
+            name: '测试'
+        };
+
+        return Response(rspData.join(''), {
+            headers: {
+                "content-type": "text/html;charset=UTF-8",
+            },
+        })
+    } catch (e) {
+        log("Got Exception: " + e.stack);
+        return Response("Got Exception: " + e.message, {
+            headers: {
+                "content-type": "text/html;charset=UTF-8",
+            },
+        });
+    }
+}
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="requestData">请求接口</button>
+  <div>{{responseData}}</div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      responseData: '',
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    requestData () {
+      axios.get('/functions/test?ID=12345')
+              .then(function (response) {
+                console.log('请求成功', response);
+                this.responseData = response;
+              })
+              .catch(function (error) {
+                console.log('请求失败', error);
+              });
+    }
+  }
+}
+</script>
+
+<style>
+</style>
